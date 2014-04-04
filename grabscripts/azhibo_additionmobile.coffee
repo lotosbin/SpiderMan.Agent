b791063f99d61f510feb1b54f68107ab2309a0e8
cheney jin <zicjin@gmail.com>
2014-04-04 19:36:13 +0800

    .

--- grabscripts/azhibo_additionmobile.coffee
+++ grabscripts/azhibo_additionmobile.coffee
@@ -1,7 +1,7 @@
 window.spGrab = ->
     caps = ['NBA','CBA','男篮世锦赛','男篮亚锦赛','德甲','西甲','意甲','英超','法甲','欧冠','世界杯','欧洲杯','美洲杯','亚洲杯','亚冠','欧联杯','足总杯','国王杯','联赛杯','意大利杯','德国杯','足协杯']
     data = []
-    $('#match-container>.box').slice(1).each ->
+    $('#match-container>.box').slice(1, 3).each ->
         day = new Date().getFullYear() + '年' + $(this).children('.title').text().trim().split(' ')[0]
         $('li.match', this).each ->
             if _.include caps, $('a.league', this).text()
--- grabscripts/azhibo_additionmobile.js
+++ grabscripts/azhibo_additionmobile.js
@@ -3,7 +3,7 @@ window.spGrab = function() {
   var caps, data;
   caps = ['NBA', 'CBA', '男篮世锦赛', '男篮亚锦赛', '德甲', '西甲', '意甲', '英超', '法甲', '欧冠', '世界杯', '欧洲杯', '美洲杯', '亚洲杯', '亚冠', '欧联杯', '足总杯', '国王杯', '联赛杯', '意大利杯', '德国杯', '足协杯'];
   data = [];
-  $('#match-container>.box').slice(1).each(function() {
+  $('#match-container>.box').slice(1, 3).each(function() {
     var day;
     day = new Date().getFullYear() + '年' + $(this).children('.title').text().trim().split(' ')[0];
     return $('li.match', this).each(function() {
