631c11179542783b1421ed343650e20625c843da
cheney <zicjin@gmail.com>
2014-06-14 09:27:44 +0800

    .

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ then
 fi
 serverUrl="http://baozoubisai.com:2083"
 phantomjs spiderman.coffee $name $serverUrl
+# http://stackoverflow.com/questions/8778513/how-can-i-setup-run-phantomjs-on-ubuntu 手动安装
